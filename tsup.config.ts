a3afe9cf2629389917b4a820aeccd363e75a3cf2
pearpages <pere@soms.cat>
2025-09-11 19:30:51 +0200

    setup: add tsup config

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],       // main entry point
+  format: ["esm"],         // build both module types
+  dts: true,                      // generate .d.ts types
+  sourcemap: true,                // useful for debugging
+  clean: true,                    // clear dist before build
+  treeshake: true,                // remove unused code
+  target: "es2022",               // modern JS output
+  external: ["react", "react-dom"] // keep as peer deps
+});
