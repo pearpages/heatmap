7d0c16d3d0dae254a1855580493861174f8377fc
Pere Pages <pere.pages@ocado.com>
2025-09-12 20:07:17 +0200

    fix: css

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,19 +1,14 @@
 import { defineConfig } from "tsup";
+import { sassPlugin } from "esbuild-sass-plugin";
 
 export default defineConfig({
-  entry: ["src/index.ts"],       // main entry point
-  format: ["esm"],         // build both module types
-  dts: true,                      // generate .d.ts types
-  sourcemap: true,                // useful for debugging
-  clean: true,                    // clear dist before build
-  treeshake: true,                // remove unused code
-  target: "es2022",               // modern JS output
-  external: ["react", "react-dom"], // keep as peer deps
-  esbuildOptions(options) {
-    // Configure loaders for different file types
-    options.loader = {
-      ...options.loader,
-      '.scss': 'css',
-    }
-  }
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  treeshake: true,
+  target: "es2022",
+  external: ["react", "react-dom"],
+  esbuildPlugins: [sassPlugin()],
 });
