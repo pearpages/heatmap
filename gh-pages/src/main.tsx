af02abaf0ff4d12dd52bb461948b5d3627a987c5
Pere Pages <pere.pages@ocado.com>
2025-09-12 21:57:45 +0200

    feat: gh-pages

--- gh-pages/src/main.tsx
+++ gh-pages/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import '@pearpages/heatmap/styles.scss';
+import '@pearpages/heatmap/styles.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
